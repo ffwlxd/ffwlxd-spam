32f38ad03ee3dcd7697e34f5caf159db69243934
ffwlxd <facebookidlock7@gmail.com>
2025-08-09 18:21:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,5 +155,5 @@ def send_requests():
 
     return jsonify(response_data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
